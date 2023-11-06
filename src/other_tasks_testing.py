12f3919aec77e4ca33524c17588254c4a51ab7c8
Arctiqa <sinitro@yandex.ru>
2023-11-06 15:53:46 +0800

    add other_tasks_testing.py, test_other_task_testing.py_

--- src/other_tasks_testing.py
+++ src/other_tasks_testing.py
@@ -1,3 +1,7 @@
+from math import pi, sqrt
+from typing import Optional
+
+
 def sum_divisible_by_3_or_5(lst: list[int]) -> int:
     """
     Функция принимает на вход список чисел и возвращает сумму всех элементов списка, которые делятся на 3 или  5 без остатка.
@@ -24,4 +28,53 @@ def check_email(email: str) -> bool:
         return True
 
 
+def count_number_in_list(lst: list[int], num: int) -> int:
+    return lst.count(num)
 
+
+def calculate_area(shape: str, sides: list[int]) -> Optional:
+    if shape.lower() in ['circle', 'круг']:
+        return pi * sides[0] ** 2
+    elif shape.lower() in ['square', 'квадрат']:
+        return sides[0] ** 2
+    elif shape.lower() in ['triangle', 'треугольник']:
+        p = sum(sides) / 2
+        a, b, c = sides
+        return sqrt(p * (p - a) * (p - b) * (p - c))
+    elif shape.lower() in ['rectangle', 'прямоугольник']:
+        return max(sides) * min(sides)
+    else:
+        return None
+
+
+def my_slice(coll, start=0, end=None):
+    """
+    Возвращает новый массив, содержащий копию части исходного массива.
+    :param coll: исходный список.
+    :param start: индекс, по которому начинается извлечение. Если индекс отрицательный,
+    start указывает смещение от конца списка. По умолчанию равен нулю.
+    :param end: индекс, по которому заканчивается извлечение (не включая элемент с индексом end).
+    Если индекс отрицательный, end указывает смещение от конца списка. По умолчанию равен длине исходного списка.
+    :return: массив элементов
+    """
+    length = len(coll)
+
+    if length == 0:
+        return []
+
+    normalized_end = length if end is None else end
+
+    normalized_start = start
+
+    if normalized_start < 0:
+        if normalized_start < -length:
+            normalized_start = 0
+        else:
+            normalized_start += length
+
+    return coll[normalized_start:normalized_end]
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(my_slice(a, -10, 7))
+print(a[0:-6])
--- tests/test_other_task_testing.py
+++ tests/test_other_task_testing.py
@@ -1,5 +1,7 @@
+from math import pi
 import pytest
-from src.other_tasks_testing import sum_divisible_by_3_or_5, check_email
+from src.other_tasks_testing import (sum_divisible_by_3_or_5, check_email,
+                                     count_number_in_list, calculate_area, my_slice)
 
 
 @pytest.mark.parametrize('lst, expected', [([3, 4, 5, 6, 7, 10], 24),
@@ -17,7 +19,9 @@ def test_check_email():
 @pytest.mark.parametrize('email, expected', [('@trrqw.tre', False),
                                              ('fafdfa@.fdgfa', False),
                                              ('rsfgg.graa@gasd', False),
-                                             ('rewq@reqer.', False)])
+                                             ('rewq@reqer.', False),
+                                             ('trww@fgs@gsd', False),
+                                             ('qwer@rqt.tqt.tqwt', False)])
 def test_check_email_with_invalid_email(email, expected):
     assert check_email(email) is expected
 
@@ -25,4 +29,52 @@ def test_check_email_with_invalid_email(email, expected):
 def test_check_email_with_empty_email():
     assert check_email('') is False
 
+
+@pytest.fixture()
+def lst_test():
+    return [1, 2, 34, 5, 3, 5, 3, 2, 65, 43, 2, 34, 57, 5, 5, 0, 0, 0]
+
+
+@pytest.fixture()
+def number_for_count():
+    return 5
+
+
+def test_count_number_in_list(lst_test, number_for_count):
+    assert count_number_in_list(lst_test, number_for_count) == 4
+
+
+def test_count_number_in_empty_list(number_for_count):
+    assert count_number_in_list([], number_for_count) == 0
+
+
+def test_count_number_out_of_number(lst_test):
+    assert count_number_in_list(lst_test, 90) == 0
+
+
+@pytest.mark.parametrize('shape, sides, expected', [('Круг', [10], pi * 100),
+                                                    ('rectangle', [3, 2, 3, 2], 6),
+                                                    ('квадрат', [5], 25),
+                                                    ('triangle', [5, 4, 3], 6),
+                                                    ('another', [4, 2, 5], None)])
+def test_calculate_area(shape, sides, expected):
+    assert calculate_area(shape, sides) == expected
+
+
+@pytest.fixture
+def lst():
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+@pytest.mark.parametrize('start, end, expected', [(3, 7, [4, 5, 6, 7]),
+                                                  (-3, -7, []),
+                                                  (-10, 7, [1, 2, 3, 4, 5, 6, 7]),
+                                                  (5, None, [6, 7, 8, 9]),
+                                                  (2, -3, [3, 4, 5, 6])])
+def test_my_slice(lst, start, end, expected):
+    assert my_slice(lst, start, end) == expected
+
+
+def test_my_slice_empty_list():
+    assert my_slice([], 3, 6) == []
 
